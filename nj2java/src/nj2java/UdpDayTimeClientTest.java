42fa8370acd9b7c039370137c6da50a60ef00715
JiHoon <JiHoon@10.20.61.203>
2015-05-27 17:44:50 +0900

    2015.05.27 UdpDayTimeClient

---
+++ nj2java/src/nj2java/UdpDayTimeClientTest.java
@@ -0,0 +1,36 @@
+package nj2java;
+import java.io.*;
+import java.net.*;
+
+public class UdpDayTimeClientTest {
+	public void start() throws IOException, UnknownHostException {
+		DatagramSocket datagramSocket = new DatagramSocket();
+		
+		byte[] msg = new byte[100];
+		
+		String data = "jh";
+		byte[]byteARR = data.getBytes("UTF-8");
+		
+		DatagramPacket outPacket = new DatagramPacket (byteARR,
+				byteARR.length,
+				new InetSocketAddress("220.149.124.244", 3000));
+		
+		DatagramPacket inPacket = new DatagramPacket(msg, msg.length);
+		
+		datagramSocket.send(outPacket);
+		
+		datagramSocket.receive(inPacket);
+		
+		System.out.println(new String(inPacket.getData()));
+		
+		datagramSocket.close();
+	} //start()
+	
+	public static void main(String args[]) {
+		try {
+			new UdpDayTimeClientTest().start();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	} //main
+}
