fadfeeae03263db0bafcac9b21fd8cdf5aa2a19b
주연짱 <@Lee-Juyeon>
2015-05-31 20:49:59 +0900

    client img & login form

---
+++ nj2java/src/Client/LoginFrame.java
@@ -0,0 +1,123 @@
+package view;
+
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.awt.event.*;
+
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLayeredPane;
+import javax.swing.JPanel;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
+import javax.swing.JOptionPane;
+
+import controller.LoginService;
+
+public class LoginFrame extends JFrame implements ActionListener {
+ 
+    BufferedImage img = null;
+    JTextField loginTextField;
+    JPasswordField passwordField;
+    JButton bt, ct, dt, et, ft;
+ 
+    public static void main(String[] args){
+        new LoginFrame();
+    }
+    
+    public LoginFrame(){
+        setTitle("로그인 테스트");
+        setSize(500, 500);
+        setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+ 
+        setLayout(null);
+        JLayeredPane layeredPane = new JLayeredPane();
+        layeredPane.setBounds(0, 0, 500, 500);
+        layeredPane.setLayout(null);
+        
+        try{
+            img = ImageIO.read(new File("img/login.jpg"));
+        } catch (IOException e){
+            System.out.println("이미지 불러오기 실패");
+            System.exit(0);
+        }
+        
+        MyPanel panel = new MyPanel();
+        panel.setBounds(0, 0, 500, 500);
+        
+        loginTextField = new JTextField(15);
+        loginTextField.setBounds(250, 200, 280, 30);
+        layeredPane.add(loginTextField);
+        loginTextField.setOpaque(false);
+        loginTextField.setForeground(Color.black);
+        loginTextField.setBorder(javax.swing.BorderFactory.createEmptyBorder());
+        
+        passwordField = new JPasswordField(15);
+        passwordField.setBounds(250, 250, 280, 30);
+        passwordField.setOpaque(false);
+        passwordField.setForeground(Color.black);
+        passwordField.setBorder(javax.swing.BorderFactory.createEmptyBorder());
+        layeredPane.add(passwordField);
+ 
+        bt = new JButton(new ImageIcon("img/btlogin_hud.jpg"));
+        bt.setBounds(197, 300, 104, 48);
+
+        bt.setBorderPainted(false);
+        bt.setFocusPainted(false);
+        bt.setContentAreaFilled(false);
+        bt.addActionListener(this);
+        layeredPane.add(bt);
+        
+        ct = new JButton(new ImageIcon("img/number.jpg"));
+        ct.setBounds(125, 190, 104, 48);
+
+        ct.setBorderPainted(false);
+        ct.setFocusPainted(false);
+        ct.setContentAreaFilled(false);
+        layeredPane.add(ct);
+        
+        dt = new JButton(new ImageIcon("img/password.jpg"));
+        dt.setBounds(125, 240, 104, 48);
+
+        dt.setBorderPainted(false);
+        dt.setFocusPainted(false);
+        dt.setContentAreaFilled(false);
+        layeredPane.add(dt);
+
+        layeredPane.add(panel);
+        add(layeredPane);
+        setVisible(true);
+    }
+ 
+    class MyPanel extends JPanel {
+        public void paint(Graphics g) {
+            g.drawImage(img, 207, 20, null);
+        }
+    }
+    
+    public void actionPerformed(ActionEvent e) {
+        String id = loginTextField.getText();
+        char[] pass = passwordField.getPassword();
+        String password = new String(pass);
+ 
+        if (id.equals("") || password.equals("")) {
+        	JOptionPane.showMessageDialog(null, "학번/비밀번호를 입력하세요.");
+        } else {
+            boolean existLogin = LoginService.loginTest(id,  password);
+ 
+            if (existLogin) {
+                JOptionPane.showMessageDialog(null, "로그인 성공");
+            } else {
+                JOptionPane.showMessageDialog(null, "로그인 실패");
+            }
+        }
+        
+        password = null;
+        
+    }
+}
--- nj2java/src/Server/ServerMain.java
+++ nj2java/src/Server/ServerMain.java
@@ -7,97 +7,84 @@ import javax.imageio.ImageIO;
 import javax.swing.*;
 
 public class ServerMain extends JFrame{
-	private JTextField txtSelectDate;
-	private JTextField txtStart;
-	private JTextField txtEnd;
-	private JTextField textField;
-	private JTextField textField_1;
 	private JButton button_1;
-	private JTextField txtAttendanceAdminSystem;
 	private JTextField textField_2;
+	private JLabel lblAttendanceAdminSystem;
+	private JLabel lblSelectDate;
+	private JLabel lblStart;
+	private JLabel lblNewLabel;
+	private JLabel label;
+	private JLabel lblEnd;
 	public ServerMain() {
 		getContentPane().setEnabled(false);
 		SpringLayout springLayout = new SpringLayout();
 		getContentPane().setLayout(springLayout);
 		
-		txtSelectDate = new JTextField();
-		springLayout.putConstraint(SpringLayout.WEST, txtSelectDate, 20, SpringLayout.WEST, getContentPane());
-		txtSelectDate.setText("Select Date");
-		getContentPane().add(txtSelectDate);
-		txtSelectDate.setColumns(10);
-		
-		txtStart = new JTextField();
-		springLayout.putConstraint(SpringLayout.SOUTH, txtSelectDate, -23, SpringLayout.NORTH, txtStart);
-		springLayout.putConstraint(SpringLayout.EAST, txtSelectDate, 0, SpringLayout.EAST, txtStart);
-		springLayout.putConstraint(SpringLayout.NORTH, txtStart, 104, SpringLayout.NORTH, getContentPane());
-		springLayout.putConstraint(SpringLayout.WEST, txtStart, 20, SpringLayout.WEST, getContentPane());
-		txtStart.setText("Start");
-		getContentPane().add(txtStart);
-		txtStart.setColumns(10);
-		
 		JButton btnStart = new JButton("Start");
-		springLayout.putConstraint(SpringLayout.WEST, btnStart, 124, SpringLayout.WEST, getContentPane());
 		springLayout.putConstraint(SpringLayout.SOUTH, btnStart, -135, SpringLayout.SOUTH, getContentPane());
-		springLayout.putConstraint(SpringLayout.EAST, txtStart, -23, SpringLayout.WEST, btnStart);
 		getContentPane().add(btnStart);
 		
-		txtEnd = new JTextField();
-		springLayout.putConstraint(SpringLayout.EAST, btnStart, -34, SpringLayout.WEST, txtEnd);
-		springLayout.putConstraint(SpringLayout.NORTH, txtEnd, 0, SpringLayout.NORTH, txtStart);
-		springLayout.putConstraint(SpringLayout.WEST, txtEnd, 235, SpringLayout.WEST, getContentPane());
-		txtEnd.setText("End");
-		getContentPane().add(txtEnd);
-		txtEnd.setColumns(10);
-		
 		JButton btnEnd = new JButton("End");
-		springLayout.putConstraint(SpringLayout.EAST, txtEnd, -27, SpringLayout.WEST, btnEnd);
+		springLayout.putConstraint(SpringLayout.EAST, btnStart, -142, SpringLayout.WEST, btnEnd);
 		springLayout.putConstraint(SpringLayout.NORTH, btnEnd, 103, SpringLayout.NORTH, getContentPane());
 		springLayout.putConstraint(SpringLayout.WEST, btnEnd, 343, SpringLayout.WEST, getContentPane());
 		springLayout.putConstraint(SpringLayout.EAST, btnEnd, -10, SpringLayout.EAST, getContentPane());
 		getContentPane().add(btnEnd);
 		
-		textField = new JTextField();
-		springLayout.putConstraint(SpringLayout.NORTH, textField, 19, SpringLayout.SOUTH, txtStart);
-		springLayout.putConstraint(SpringLayout.WEST, textField, 0, SpringLayout.WEST, txtStart);
-		springLayout.putConstraint(SpringLayout.EAST, textField, 0, SpringLayout.EAST, txtStart);
-		textField.setText("\uC0C1\uD669");
-		getContentPane().add(textField);
-		textField.setColumns(10);
-		
 		JButton button = new JButton("\uC0C1\uD669");
-		springLayout.putConstraint(SpringLayout.NORTH, button, -1, SpringLayout.NORTH, textField);
+		springLayout.putConstraint(SpringLayout.NORTH, button, 16, SpringLayout.SOUTH, btnStart);
 		springLayout.putConstraint(SpringLayout.WEST, button, 0, SpringLayout.WEST, btnStart);
 		springLayout.putConstraint(SpringLayout.EAST, button, -229, SpringLayout.EAST, getContentPane());
 		getContentPane().add(button);
 		
-		textField_1 = new JTextField();
-		springLayout.putConstraint(SpringLayout.NORTH, textField_1, 22, SpringLayout.SOUTH, textField);
-		springLayout.putConstraint(SpringLayout.WEST, textField_1, 20, SpringLayout.WEST, getContentPane());
-		springLayout.putConstraint(SpringLayout.EAST, textField_1, 0, SpringLayout.EAST, txtStart);
-		textField_1.setText("\uBA54\uC138\uC9C0\uD568");
-		getContentPane().add(textField_1);
-		textField_1.setColumns(10);
-		
 		button_1 = new JButton("\uBA54\uC138\uC9C0\uD568");
-		springLayout.putConstraint(SpringLayout.NORTH, button_1, -1, SpringLayout.NORTH, textField_1);
+		springLayout.putConstraint(SpringLayout.NORTH, button_1, 20, SpringLayout.SOUTH, button);
 		springLayout.putConstraint(SpringLayout.EAST, button_1, 0, SpringLayout.EAST, button);
 		getContentPane().add(button_1);
 		
-		txtAttendanceAdminSystem = new JTextField();
-		springLayout.putConstraint(SpringLayout.NORTH, txtAttendanceAdminSystem, 10, SpringLayout.NORTH, getContentPane());
-		springLayout.putConstraint(SpringLayout.WEST, txtAttendanceAdminSystem, 0, SpringLayout.WEST, btnStart);
-		springLayout.putConstraint(SpringLayout.EAST, txtAttendanceAdminSystem, -123, SpringLayout.EAST, getContentPane());
-		txtAttendanceAdminSystem.setText("Attendance Admin System");
-		getContentPane().add(txtAttendanceAdminSystem);
-		txtAttendanceAdminSystem.setColumns(10);
-		
 		textField_2 = new JTextField();
-		springLayout.putConstraint(SpringLayout.WEST, textField_2, 23, SpringLayout.EAST, txtSelectDate);
+		springLayout.putConstraint(SpringLayout.WEST, textField_2, 124, SpringLayout.WEST, getContentPane());
 		springLayout.putConstraint(SpringLayout.SOUTH, textField_2, -22, SpringLayout.NORTH, btnStart);
-		springLayout.putConstraint(SpringLayout.EAST, textField_2, 139, SpringLayout.EAST, txtSelectDate);
+		springLayout.putConstraint(SpringLayout.EAST, textField_2, -194, SpringLayout.EAST, getContentPane());
 		textField_2.setText("2015 / 04 / 19");
 		getContentPane().add(textField_2);
 		textField_2.setColumns(10);
+		
+		lblAttendanceAdminSystem = new JLabel("Attendance Admin System");
+		springLayout.putConstraint(SpringLayout.NORTH, lblAttendanceAdminSystem, 10, SpringLayout.NORTH, getContentPane());
+		springLayout.putConstraint(SpringLayout.EAST, lblAttendanceAdminSystem, -118, SpringLayout.EAST, getContentPane());
+		lblAttendanceAdminSystem.setFont(new Font("±¼¸²", Font.PLAIN, 17));
+		getContentPane().add(lblAttendanceAdminSystem);
+		
+		lblSelectDate = new JLabel("Select Date");
+		springLayout.putConstraint(SpringLayout.WEST, lblSelectDate, 20, SpringLayout.WEST, getContentPane());
+		springLayout.putConstraint(SpringLayout.EAST, lblSelectDate, -35, SpringLayout.WEST, textField_2);
+		springLayout.putConstraint(SpringLayout.NORTH, lblSelectDate, 3, SpringLayout.NORTH, textField_2);
+		getContentPane().add(lblSelectDate);
+		
+		lblStart = new JLabel("Start");
+		springLayout.putConstraint(SpringLayout.WEST, btnStart, 49, SpringLayout.EAST, lblStart);
+		springLayout.putConstraint(SpringLayout.NORTH, lblStart, 4, SpringLayout.NORTH, btnStart);
+		springLayout.putConstraint(SpringLayout.WEST, lblStart, 40, SpringLayout.WEST, getContentPane());
+		springLayout.putConstraint(SpringLayout.EAST, lblStart, 75, SpringLayout.WEST, getContentPane());
+		getContentPane().add(lblStart);
+		
+		lblNewLabel = new JLabel("\uC0C1\uD669");
+		springLayout.putConstraint(SpringLayout.NORTH, lblNewLabel, 1, SpringLayout.NORTH, button);
+		springLayout.putConstraint(SpringLayout.WEST, lblNewLabel, 0, SpringLayout.WEST, lblStart);
+		springLayout.putConstraint(SpringLayout.SOUTH, lblNewLabel, 41, SpringLayout.SOUTH, lblStart);
+		springLayout.putConstraint(SpringLayout.EAST, lblNewLabel, 109, SpringLayout.WEST, getContentPane());
+		getContentPane().add(lblNewLabel);
+		
+		label = new JLabel("\uBA54\uC138\uC9C0\uD568");
+		springLayout.putConstraint(SpringLayout.NORTH, label, 4, SpringLayout.NORTH, button_1);
+		springLayout.putConstraint(SpringLayout.EAST, label, 0, SpringLayout.EAST, lblStart);
+		getContentPane().add(label);
+		
+		lblEnd = new JLabel("End");
+		springLayout.putConstraint(SpringLayout.NORTH, lblEnd, 4, SpringLayout.NORTH, btnStart);
+		springLayout.putConstraint(SpringLayout.EAST, lblEnd, -42, SpringLayout.WEST, btnEnd);
+		getContentPane().add(lblEnd);
 	}
 
 	public static void main(String[] args) {
