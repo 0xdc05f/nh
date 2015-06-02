f547333fde6b366e78d5e150b01da1acbdaa44d7
주연짱 <@10.20.60.243>
2015-06-02 23:35:10 +0900

    Ssd

--- nj2java/src/Server/AttendanceSitFrame.java
+++ nj2java/src/Server/AttendanceSitFrame.java
@@ -10,13 +10,18 @@ class AttendanceSitFrame{
 	JFrame jFrame = new JFrame("출석상황");
 	
 	String columnName[]=
-		{"사진", "이름", "학번", "소속학과", "출석", "지각", "결석"};
+		{"이름", "학번", "소속학과", "출석"};
+		/*{"사진", "이름", "학번", "소속학과", "출석", "지각", "결석"};*/
 	Object rowData[][]=
 		{
-			{new ImageIcon("default/a.jpg"), "강태민", "14113303", "산업공학과", "결석"},
+			{"강태민", "14113303", "산업공학과", "결석"},
+			{"이주연", "14113346", "산업공학과", "출석"},
+			{"지한별", "14113360", "산업공학과", "지각"},
+			{"홍서우", "14113365", "산업공학과", "지각"}
+			/*{new ImageIcon("default/a.jpg"), "강태민", "14113303", "산업공학과", "결석"},
 			{new ImageIcon("default/b.jpg"), "이주연", "14113346", "산업공학과", "출석"},
 			{new ImageIcon("default/c.jpg"), "지한별", "14113360", "산업공학과", "지각"},
-			{new ImageIcon("default/d.jpg"), "홍서우", "14113365", "산업공학과", "지각"}
+			{new ImageIcon("default/d.jpg"), "홍서우", "14113365", "산업공학과", "지각"}*/
 		};
 
 	JTable jTable = new JTable(rowData, columnName);
@@ -25,12 +30,12 @@ class AttendanceSitFrame{
 	public AttendanceSitFrame(){
 
 		jFrame.add(jScrollPane);
-		jFrame.setSize(700,500);
-	//	jFrame.setLayout(null);
-	//	jFrame.setTitle("출석상황");
-		jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		jFrame.setSize(400, 300);
 		jFrame.setVisible(true);
+		jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
+
+
 
 
 }
