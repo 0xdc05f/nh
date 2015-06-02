27ff5dbc0b9f8ea255ddf98d2023130ee6d8c73c
주연짱 <@10.20.60.243>
2015-06-02 23:24:55 +0900

    dddddd

---
+++ nj2java/src/Server/MessageAction.java
@@ -0,0 +1,17 @@
+package Server;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+
+//출석상황 이벤트
+class MessageAction implements ActionListener{
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		JButton b = (JButton)e.getSource();
+		if(e.getSource()==b)
+			new AttendanceSitFrame();	
+	}
+
+}
