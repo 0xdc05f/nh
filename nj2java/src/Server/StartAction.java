31dfe7bc88b4d2efd1d24764d8ecd7a60b1e3558
0xdc05f <namhun@10.20.63.119>
2015-06-15 21:46:00 +0900

    서

--- nj2java/src/Server/StartAction.java
+++ nj2java/src/Server/StartAction.java
@@ -1,16 +1,76 @@
 package Server;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Collections;
+import java.util.HashMap;
 
-import javax.swing.JButton;
 
 //출석상황 이벤트
 class StartAction implements ActionListener{
+	private HashMap<String, DataOutputStream> clients;
+	private ServerSocket serverSocket;
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		// TODO Auto-generated method stub
+		new StartAction().start();
 		
 	}
+	public StartAction() {
+        clients = new HashMap<String, DataOutputStream>();
+ 
+        // 스레드 동기
+        Collections.synchronizedMap(clients);
+    }
+	
+	public void start(){
+		try{
+			Socket socket;
+			//소켓 생
+			serverSocket = new ServerSocket(5000);
+			System.out.println("서버 시작");
+			
+			while(true){
+				socket = serverSocket.accept();
+				ServerReceiver receiver = new ServerReceiver(socket);
+				System.out.println("sdfsad");
+				//receiver.start();
+			}
+		}catch(IOException e){
+			e.printStackTrace();
+		}
+	}
+	
+	class ServerReceiver extends Thread{
+		Socket socket;
+		DataInputStream input;
+		DataOutputStream output;
+		
+		public ServerReceiver(Socket socket){
+			this.socket = socket;
+			try{
+				input = new DataInputStream(socket.getInputStream());
+				output = new DataOutputStream(socket.getOutputStream());
+			}catch(IOException e){
+				
+			}
+		}
+		
+		public void run(){
+			String name = "";
+			try {
+				name = input.readUTF();
+			}catch(IOException e){
+				
+			}finally{
+				
+			}
+		}
+	}
+	
 
 }
