d5584fdac2b2e146c63466dd55aa3595dc0c237b
CSS.J <CSS.J@220.149.125.131>
2015-06-16 18:11:31 +0900

    yy

--- nj2java/src/Server/StartAction.java
+++ nj2java/src/Server/StartAction.java
@@ -1,25 +1,27 @@
 package Server;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.Collections;
-import java.util.HashMap;
-
+import java.util.HashMap; //패키지 설명
+import java.util.Iterator; //패키지 설명
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.StringTokenizer;
 
 //출석상황 이벤트
-class StartAction implements ActionListener{
+public class StartAction implements ActionListener{
 	private HashMap<String, DataOutputStream> clients;
 	private ServerSocket serverSocket;
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		new StartAction().start();
-		
 	}
+	
 	public StartAction() {
         clients = new HashMap<String, DataOutputStream>();
  
@@ -30,17 +32,21 @@ class StartAction implements ActionListener{
 	public void start(){
 		try{
 			Socket socket;
-			//소켓 생
-			serverSocket = new ServerSocket(5000);
+			
+			//소켓 생성
+			serverSocket = new ServerSocket(3000);
 			System.out.println("서버 시작");
 			
+			//클라이언트와 연결되면
 			while(true){
+				//통신 소켓 생성하고 스레드 생성
 				socket = serverSocket.accept();
 				ServerReceiver receiver = new ServerReceiver(socket);
-				System.out.println("sdfsad");
-				//receiver.start();
+				System.out.println("실행");
+				receiver.start();
 			}
-		}catch(IOException e){
+		}
+		catch(IOException e){
 			e.printStackTrace();
 		}
 	}
@@ -56,23 +62,35 @@ class StartAction implements ActionListener{
 				input = new DataInputStream(socket.getInputStream());
 				output = new DataOutputStream(socket.getOutputStream());
 			}catch(IOException e){
-				
+				System.out.println("오류1");
 			}
 		}
 		
-
-		
 		public void run(){
-			String name = "";
+			String idpw = null;
+			
+			StringTokenizer st = new StringTokenizer(idpw, "|");
+			while(st.hasMoreTokens()){
+				String temp = st.nextToken();
+				if(st.equals("|")){
+					System.out.println("\n");
+				}else{
+					System.out.println(temp);
+				}
+			}
+			
 			try {
-				name = input.readUTF();
+				idpw = input.readUTF();
+				
+                clients.put(idpw, output);
+                //System.out.println("현재 " + clients.size() + "명이 대화방에 접속 중입니다.");
+
 			}catch(IOException e){
-				
 			}finally{
-				
+				clients.remove(idpw);
+                
+				System.out.println(idpw + "\n" + "[" + socket.getInetAddress() + ":" + socket.getPort() + "]" + "님이 대화방에서 나갔습니다.");
 			}
 		}
 	}
-	
-
 }
