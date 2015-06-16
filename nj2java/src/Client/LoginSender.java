491b2c994f389e835cdbc2f0810c3d01ddae61e0
주연짱 <@10.20.62.236>
2015-06-16 15:00:33 +0900

    client

--- nj2java/src/Client/LoginSender.java
+++ nj2java/src/Client/LoginSender.java
@@ -7,21 +7,21 @@ import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 import java.net.Socket;
 import java.util.Scanner;
- 
+
 public class LoginSender extends LoginFrame {
-    private String name;
+    private String login;
     private Socket socket;
-    private String serverIp = "10.20.63.119";
+    private String serverIp = "220.149.125.131";
     
     //LoginFrame 상속받아서 가져오기
 	String id = loginTextField.getText().trim();
-	String pw=new String(passwordField.getPassword());
+	String pw = new String(passwordField.getPassword());
 
     
     public void start() {
         try {
             socket = new Socket(serverIp, 5000);
-            System.out.println("서버와 연결되었습니다.");
+            System.out.println("서버와 연결되었습니다. 대화명을 입력하세요 : ");
             ClientSender clientSender = new ClientSender(socket);
             clientSender.start();
         } catch (IOException e) {System.out.println("연결실패");}
@@ -29,9 +29,8 @@ public class LoginSender extends LoginFrame {
  
     class ClientSender extends Thread {
    	
-
     	// 소켓을 만들기 위한 인스턴스들
-    	private Socket socket=null;
+    	Socket socket;
     	
     	// 소켓에서 스트림을 얻어오기 위한 인스턴스들
     	private OutputStream os = null;
@@ -47,9 +46,9 @@ public class LoginSender extends LoginFrame {
  
         @Override
         public void run() {
-       	  try{
-             writer.write(id,0,10);
-             writer.write(pw,0,10);
+        	try{
+        	login = 
+             writer.write(id+"|"+pw,0,(id+"|"+pw).length());
              writer.flush();
           }catch(IOException e){System.out.println("보내기 실패");}
               
@@ -57,7 +56,24 @@ public class LoginSender extends LoginFrame {
              socket.close();
              writer.close();
              os.close();
-          }catch(IOException e){ }
-        }
+          }catch(IOException e){
+          }
+    	}
+	  }
+	}
+    
+    class ClientSender extends Thread{
+    	Socket socket;
+    	DataOutputStream output;
+    	
+    	public ClientSender(Socket socket){
+    		this.socket = socket;
+    		try{
+    			output = new DataOutputStream(socket.getOutputStream());
+    			output.writeUTF(login);
+    			System.out.println("대화방에 입장하였습니다.");
+    		}catch(Exception e){
+    		}
+    	}
     }
-}
+
