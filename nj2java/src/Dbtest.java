f6108e4ce4dbbb229cc08c196a6a8c1e4398d453
주연짱 <@10.20.62.236>
2015-06-17 16:05:09 +0900

    dd

--- nj2java/src/Dbtest.java
+++ nj2java/src/Dbtest.java
@@ -1,7 +1,28 @@
-import SQLite.JDBCDriver;
-import java.sql;
+import java.sql.*;
+import java.io.*;
 
+public class Dbtest{
+	
 
-public class Dbtest {
-
+	public static void main(String args[]) throws Exception{
+		Connection connection = null;
+		Statement statement = null;
+		
+		try{
+			Class.forName("org.sqlite.JDBC");
+		}catch(ClassNotFoundException e){
+			System.out.println("org.sqlite.JDBC룰 찾지 못했습니다.");
+		}
+		
+		connection = DriverManager.getConnection("/bin/nj2.db");
+		statement = connection.createStatement();
+		
+		ResultSet resultSet = statement.executeQuery("SELECT *"+"FROM member");
+	
+		while(resultSet.next()){
+			//printWriter.printf("\"%s\", \"%s\", \"%s\". \"%d\", \"%d\", \"%d\"\n", new Object[]{resultSet.getString("field1"), new Integer(resultSet.getInt("field2"))});
+		}
+		resultSet.close();
+		connection.close();
+	}
 }
